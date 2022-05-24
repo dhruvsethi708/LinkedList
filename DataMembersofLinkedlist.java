55ecc6af878b5e2e21a26d150141d55406e0d716
Dhruv Sethi <74965209+dhruvsethi708@users.noreply.github.com>
2022-05-24 16:43:02 +0530

    initial commit

---
+++ DataMembersofLinkedlist.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class DataMembersofLinkedlist{
+    public static class Node {
+        int data;
+        Node next;
+    }
+
+    public static class LinkedList{
+        Node head;
+        Node tail;
+        int size;
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
